e31856f7d13c10a08161f0436782c2cf03d4a4a7
janekVonLipinski <janekvonlipinski222@gmail.com>
2024-10-14 15:48:52 +0200

    fixed bug in excel write

--- src/main/java/Looping/CSVFile.java
+++ src/main/java/Looping/CSVFile.java
@@ -9,22 +9,21 @@ public class CSVFile {
     public void cretateCsvFile(String PATH, Looping looping) {
 
         try (FileWriter fileWriter = new FileWriter(PATH + "LoopingDaten.csv")) {
-            fileWriter.write("X;Y\n");
 
             List<Punkt> punkte = looping.getPunkte();
             List<Double> anpressDruecke = looping.getAnpressdruck();
             List<Double> zentrifugalKraft = looping.getZentrifugalKraefte();
             List<Double> normalKraft = looping.getNormalkraft();
 
-            StringBuilder result = new StringBuilder("X;Y;F_A;F_Z;F_N;alpha");
+            StringBuilder result = new StringBuilder("X;Y;F_A;F_Z;F_N;alpha\n");
 
             for (int i = 0; i < punkte.size(); i++) {
                 Punkt punkt = punkte.get(i);
                 double anpress = anpressDruecke.get(i);
                 double zen = zentrifugalKraft.get(i);
                 double kraft = normalKraft.get(i);
-                result.append("%.8f;%.8f,%.8f,%.8f,%.8f,%.8f\n".formatted(
-                        punkt.getX(), punkt.getY(), anpress, zen, kraft, punkt.getAlpha()
+                result.append("%.8f;%.8f;%.8f;%.8f;%.8f;%.8f\n".formatted(
+                        punkt.getX(), punkt.getY(), anpress, zen, kraft, punkt.getBeta()
                 ));
             }
 
